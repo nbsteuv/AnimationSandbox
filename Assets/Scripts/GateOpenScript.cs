450d5878d35d1712a7e9a70256a74c2fcc5b9c10
nbsteuv <nbsteuv@gmail.com>
2017-02-26 09:40:42 -0500

    Gate opens and closes on box collider trigger

---
+++ Assets/Scripts/GateOpenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateOpenScript : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        anim.SetBool("IsOpening", true);
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        anim.SetBool("IsOpening", false);
+    }
+}
