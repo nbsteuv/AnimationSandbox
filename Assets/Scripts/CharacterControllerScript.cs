539b1a68b41cf5c48b049f9c84b61c2cffef6373
nbsteuv <nbsteuv@gmail.com>
2017-02-26 10:23:33 -0500

    Added character controller script

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 100f;
+  
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float forwardMotion = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+	    float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Translate(0, 0, forwardMotion);
+        transform.Rotate(0, rotation, 0);
+	}
+}
