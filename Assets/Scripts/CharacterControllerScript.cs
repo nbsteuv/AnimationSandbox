5d9cb25c48151e088d9fb6e05b27714553e4ec8e
nbsteuv <nbsteuv@gmail.com>
2017-02-27 06:17:36 -0500

    Added walk/run blend tree with R key press

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -6,7 +6,12 @@ public class CharacterControllerScript : MonoBehaviour
 {
     public float speed = 5f;
     public float rotationSpeed = 100f;
+    public float acceleration = 2.0f;
+    public float deceleration = 2.0f;
+    public float speedMax = 20f;
 
+    private float currentSpeed = 0.0f;
+    private bool isRunning = false;
     private Animator anim;
 
 	// Use this for initialization
@@ -18,23 +23,54 @@ public class CharacterControllerScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	    float forwardMotion = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-	    if (forwardMotion != 0)
+	    if (isRunning && anim.GetBool("IsWalking"))
+	    {
+	        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
+	        {
+	            currentSpeed += (Input.GetAxis("Vertical") * currentSpeed + acceleration) * Time.deltaTime;
+	            if (currentSpeed > speedMax)
+	            {
+	                currentSpeed = speedMax;
+	            }
+	        }
+	        else
+	        {
+                currentSpeed -= deceleration * Time.deltaTime;
+                if (currentSpeed < 0)
+                {
+                    currentSpeed = 0;
+                }
+            }
+	    }
+	    else
+	    {
+	        currentSpeed = Input.GetAxis("Vertical") * speed;
+        }
+	    if (currentSpeed != 0)
 	    {
 	        anim.SetBool("IsWalking", true);
-            anim.SetFloat("CharacterSpeed", forwardMotion);
+            anim.SetFloat("CharacterSpeed", currentSpeed);
 	    }
 	    else
 	    {
 	        anim.SetBool("IsWalking", false);
 	    }
+
+	    float forwardMotion = currentSpeed * Time.deltaTime;
 	    float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
         transform.Translate(0, 0, forwardMotion);
         transform.Rotate(0, rotation, 0);
 
 	    if (Input.GetButtonDown("Jump"))
 	    {
 	        anim.SetTrigger("IsJumping");
+	    }
+
+	    if (Input.GetKeyDown("r"))
+	    {
+	        isRunning = !isRunning;
+	        anim.SetBool("IsRunning", true);
 	    }
 	}
 }
