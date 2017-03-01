a3e0fbf5285666447bbeebfae7ea34ed2d950049
nbsteuv <nbsteuv@gmail.com>
2017-03-01 07:45:47 -0500

    Added sitting animation variation and mind control script

---
+++ Assets/Scripts/Controllable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllable : MonoBehaviour
+{
+    private bool selected = false;
+    private Animator anim;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        getControl();
+    }
+
+    void getControl()
+    {
+        selected = true;
+    }
+
+}
