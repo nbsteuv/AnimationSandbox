f424e801e3cc87e042fa3aa649edc0326ed7972e
nbsteuv <nbsteuv@gmail.com>
2017-03-05 10:58:55 -0500

    Scripted movement for selected character

--- Assets/Scripts/Controllable.cs
+++ Assets/Scripts/Controllable.cs
@@ -1,25 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Resources;
 using UnityEngine;
 
 public class Controllable : MonoBehaviour
 {
     public GameObject haloPrefab;
     public Vector3 haloOffset;
+    public float speed;
 
     private GameControllerScript gameController;
+    private Animator anim;
     private bool active = false;
     private GameObject halo = null;
+
+    private Vector3 targetPosition;
+    private float moveStartTime;
+    private Vector3 moveStartPosition;
+
     
 	// Use this for initialization
 	void Start ()
 	{
 	    gameController = GameObject.Find("GameController").GetComponent<GameControllerScript>();
+	    anim = GetComponent<Animator>();
+	    targetPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (targetPosition != transform.position)
+	    {
+	        anim.SetBool("IsWalking", true);
+	        float percentageCovered = ((Time.time - moveStartTime) * speed) / Vector3.Distance(moveStartPosition, targetPosition);
+            Debug.Log(percentageCovered);
+	        transform.position = Vector3.Lerp(moveStartPosition, targetPosition, percentageCovered);
+	    }
+	    else
+	    {
+            anim.SetBool("IsWalking", false);
+        }
 	}
 
     void OnMouseDown()
@@ -37,6 +57,16 @@ public class Controllable : MonoBehaviour
     {
         DestroyObject(halo);
         active = false;
+    }
+
+    public void walk(float locationX, float locationZ)
+    {
+        Debug.Log("Walk called");
+        moveStartTime = Time.time;
+        moveStartPosition = transform.position;
+        Debug.Log("Move start position: " + moveStartPosition);
+        targetPosition = new Vector3(locationX, transform.position.y, locationZ);
+        Debug.Log("Target position: " + targetPosition);
     }
 
 }
--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -14,7 +14,23 @@ public class GameControllerScript : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (Input.GetButtonDown("Fire1"))
+	    {
+	        if (activeCharacter != null)
+	        {
+                Debug.Log("Active character is not null.");
+	            Vector3 mousePosition = Input.mousePosition;
+	            float mousePositionX = mousePosition.x;
+	            float mousePositionZ = mousePosition.z;
+                Debug.Log("Mouse position: " + mousePosition);
+                Controllable activeCharacterControllerScript = activeCharacter.GetComponent<Controllable>();
+	            if (activeCharacterControllerScript != null)
+	            {
+                    Debug.Log("Calling active character script walk");
+                    activeCharacterControllerScript.walk(mousePositionX, mousePositionZ);
+                }
+	        }
+	    }
 	}
 
     public void setActiveCharacter(GameObject newActiveCharacter)
